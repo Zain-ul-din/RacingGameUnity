4d981f1491f80aa3831f43aef94b2a137938bee0
Zain ul din <78583049+Zain-ul-din@users.noreply.github.com>
2022-09-01 00:37:24 +0500

    init

---
+++ TrafficeSystem/Assets/Highway Racer/Scripts/HR_ButtonSound.cs
@@ -0,0 +1,28 @@
+﻿//----------------------------------------------
+//           	   Highway Racer
+//
+// Copyright © 2014 - 2021 BoneCracker Games
+// http://www.bonecrackergames.com
+//
+//----------------------------------------------
+
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+[RequireComponent(typeof(Button))]
+public class HR_ButtonSound : MonoBehaviour, IPointerClickHandler{
+
+	private AudioSource clickSound;
+
+	public void OnPointerClick(PointerEventData data){
+
+		if (Camera.main != null) {
+			clickSound = CreateAudioSource.NewAudioSource (Camera.main.gameObject, HighwayRacerProperties.Instance.buttonClickAudioClip.name, 0f, 0f, 1f, HighwayRacerProperties.Instance.buttonClickAudioClip, false, true, true);
+			clickSound.ignoreListenerPause = true;
+			clickSound.ignoreListenerVolume = true;
+		}
+
+	}
+
+}
